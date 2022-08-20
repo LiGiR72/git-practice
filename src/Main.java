ebecc34f27f2efe6a15a67ed55e51d69ada6fe4c
LiGiR72 <iliya.alabyew@gmail.com>
2022-08-20 15:53:26 +0300

    README added, class SalesManager and its test in main added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int[] sales = {5, 10 , 8, 2};
+       SalesManager salesManager = new SalesManager(sales);
+       System.out.println(salesManager.max());
     }
 }
