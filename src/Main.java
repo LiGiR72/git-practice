d54a3f45ee39f56513b33852bee1781ab6a2e512
LiGiR72 <iliya.alabyew@gmail.com>
2022-08-28 13:41:55 +0300

    mend
    Stat method fixec

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       long[] sales = {10, 10, 10, 10, 10};
-       SalesManager salesManager = new SalesManager(sales);
-       System.out.println(salesManager.max());
-       System.out.println(salesManager.stat());
+        long[] sales = {10, 10, 10, 10, 10};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
+        System.out.println(salesManager.stat());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,24 +14,25 @@ public class SalesManager {
         }
         return max;
     }
-    public long stat(){
-        long  minIndex = 0;
+
+    public long stat() {
+        long minIndex = 0;
         long maxIndex = 0;
         long meridian = 0;
-        for(int i = 0; i < sales.length; i++){
-            if (sales[i] > maxIndex ) {
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > maxIndex) {
                 maxIndex = i;
             }
             if (sales[i] <= minIndex) {
                 minIndex = sales[i];
             }
         }
-        for(int i = 0; i < sales.length; i++){
-            if(i == maxIndex || i == minIndex){
+        for (int i = 0; i < sales.length; i++) {
+            if (i == maxIndex || i == minIndex) {
                 continue;
             }
             meridian += sales[i];
         }
-        return meridian/(sales.length - 2);
+        return meridian / (sales.length - 2);
     }
 }
